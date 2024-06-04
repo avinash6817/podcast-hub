dbaac166c1aee7b4448748b111dee162623be82a
Avinashkumar <avinashncs80@gmail.com>
2024-06-05 05:19:35 +0530

    added all files

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://proud-cattle-3.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
